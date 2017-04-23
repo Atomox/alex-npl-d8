ae20dbe93879af33af6872065a44e44c4d39b169
helmebe <helmebe@Helpdesks-MBP-8.fios-router.home>
2017-04-23 15:59:42 -0400

    author: bhelmer -- Alexa Response Algorythm -- WORKING for select content!

--- alexa-d8/web/modules/custom/npd_alexa/src/Controller/AlexaApiController.php
+++ alexa-d8/web/modules/custom/npd_alexa/src/Controller/AlexaApiController.php
@@ -4,6 +4,9 @@ namespace Drupal\npd_alexa\Controller;
 
 // use Drupal\npd_alexa\Utility\SomethingTemplateTrait;
 use Symfony\Component\HttpFoundation\Response;
+use Drupal\node\Entity\Node;
+use Drupal\node\Entity;
+use Drupal\Core\Field\Plugin\Field;
 
 class AlexaException extends \Exception {};
 
@@ -67,8 +70,8 @@ class AlexaApiController {
 */
 
 			$response = new Response();
-			$response->setContent(json_encode($answer));
 			$response->headers->set('Content-Type', 'application/json');
+			$response->setContent(json_encode($answer));
 
 			return $response;
 		}
@@ -88,13 +91,19 @@ class AlexaApiController {
 	 *   Final response, to be JSON encoded.
 	 */
 	protected function findAlexaResponse($question) {
-		syslog(1, 'Searching for question array.');
+		syslog(1, '');
+		syslog(1, ' > Begin -----------------------');
+		syslog(1, ' > Searching for question array.');
 
 		// Get our terms to search.
 		$terms = $this->filterAlexaRequest($question);
 
+		syslog(1, ' > Terms: ' . print_r($terms, TRUE));
+
 		// Build a query string.
 		$query = $this->assembleSolrQuery($terms);
+
+		syslog(1, ' > SOLR Query: ' . print_r($query, TRUE));
 
 		// Perform the search.
 		$result = $this->getSolrResponse($query);
@@ -102,18 +111,21 @@ class AlexaApiController {
 		// Clean up the response.
 		$analysis = $this->analyzeSolrResponse($result);
 
-		if ($analysis !== FALSE) {
+		// Choose final response.
+		$final = $this->selectAlexaResponse($analysis, $terms);
+
+		if ($analysis !== FALSE && isset($_GET['debug']) && strtolower($_GET['debug']) === 'true') {
 			dsm('<pre>' . print_r($analysis, TRUE) . '</pre>');
 		}
 
 		// Send the response.
-		return [
-		  "value" => $result,
+		return array(
+		  "value" => $final,
 		  "status" => ($result !== NULL) ? 0 : 1,
 		  "q" => $query,
 		  "question" => $question,
 		  "terms" => $terms,
-		];
+		);
 	}
 
 
@@ -238,6 +250,343 @@ class AlexaApiController {
 		$q = implode('%20', $q);
 
 		return $q;
+	}
+
+
+	/**
+	 
+
+
+
+
+
+	 	 @TODO
+
+			IN PROGRESS
+
+
+
+
+	 */
+	protected function selectAlexaResponse($analysis, $terms) {
+//		dsm($analysis);
+
+		syslog(1, ' > Select Alexa Response...');
+
+		$response = null;
+		$entity = null;
+		$entity_field = null;
+
+		// Select the first result, and load the entity.
+		if (is_array($analysis) && isset($analysis[0])) {
+			syslog(1, ' > Select Alexa Response > Result: ' . print_r($analysis[0], TRUE));
+
+
+			switch ($analysis[0]['bundle']) {
+
+				case 'node':
+					syslog(1, ' > Loading node id: ' . $analysis[0]['id']);
+					$entity = node::load($analysis[0]['id']);
+					$entity_field = 'body';
+
+					dsm($entity->title->value);
+					dsm($entity->body->value);
+
+					break;
+
+				default:
+					syslog(1, ' > Select Alexa Response > Entity Type: ' . $analysis[0]['bundle']);
+			}
+		}
+		else {
+			syslog(1, ' > Select Alexa Response...' . 'None found.');
+		}
+
+		// Look at the body.
+		if ($entity !== null && $entity_field !== null) {
+			if ($entity_field == 'body') {
+				$response = $entity->body->value;
+			}
+		}
+
+		// Check body for approprate response.
+		if ($response !== null) {
+			$response = $this->selectAlexaResponseContext($response, $terms);
+
+			return strip_tags($response);
+		}
+
+
+		return null;
+	}
+
+
+	protected function selectAlexaResponseContext($body, $terms) {
+		$response = '';
+
+		syslog(1, ' > Select Alexa Response: Body: ' . $body);
+
+
+		$type = ['h1', 'h2', 'h3', 'h4', 'alexa'];
+		// Search h1, h2, h3
+		foreach ($type AS $tag) {
+			$results[$tag] = $this->findAlexaResponseTags($tag, $body);
+		}
+
+//		dsm($results);
+		syslog(1, ' > Possible responses: ' . print_r($results, TRUE));
+
+		// Pick the best result.
+		$response = $this->weightResponseTags($results, $terms);
+
+		syslog(1, ' > Final Response: ' . print_r($response['response'], TRUE));
+
+		return $response['response'];
+	}
+
+
+	protected function findAlexaResponseTags($type="alexa", $body) {
+		$pattern = '';
+		$answer_position = 0;
+		$filter_short = true;
+
+
+		//
+		// @TODO
+		//
+		// Get any context back to the previous <h*> tag, or other break.
+		// Use it to provide context.
+
+		// Concept:
+		//   When multiple tag-types are adjacent, group them. Then, select between them with whichever has the highest score.
+
+		/**
+
+
+
+		   @TODO
+
+
+
+		 */
+
+
+    $pattern_prefix = '#';
+		$pattern_context = '((?:(?:<[^>]*>(?:[^<]*)(?:<[^>]*>?)*)){0,4})';
+		$pattern_suffix = '#is';
+
+		switch ($type) {
+			case 'alexa':
+
+				// Update 2 (For tags inside span)
+				$pattern = '<span\s?class="alexa"\E\s?(?:.[^<>]*)?>((?:(?!</span>).)*)</span>';
+
+/**
+ 
+// Regex:
+((?:(?:<[^>]*>(?:[^<]*)(?:<[^>]*>?)*)){0,4})
+<span\s?class="alexa"\E\s?(?:.[^<>]*)?>((?:(?!</span>).)*)</span>
+
+// Update 1
+				$pattern = '<span\s?\Qclass="alexa"\E\s?(?:.[^<>]*)?>(.[^<>]*)</span>';
+
+// Original
+				$pattern = '#<span[^>]*class="alexa">(.[^<>]*)</span>#is';
+*/
+
+				$answer_position = 1;
+				$filter_short = false;
+				break;
+
+			case 'h1':
+			case 'h2':
+			case 'h3':
+			case 'h4':
+			case 'h5':
+			case 'h6':
+							//		 <h3           (  [^>])*>((?!</    h3       >).)*<\/h3>
+				$pattern = '<' . $type . '(?:[^>])*>((?:(?!</' . $type . '>).)*)</' . $type .'>';
+				$answer_position = 1;
+				break;
+
+			default:
+				return false;
+		}
+
+		// Build pattern.
+		$pattern = $pattern_prefix . $pattern_context . $pattern . $pattern_suffix;
+
+		// Since we add a capture group for conext, bump the position down by to accomodate.
+		$answer_position += 1;
+
+
+		// Match all tags.
+		preg_match_all($pattern, $body, $matches);
+//		syslog(1, ' >>>>>>>>>>>>>>>>>>>>>>>>');
+
+//		if ($type == 'alexa') {
+//			syslog(1, ' > PREGMATCH ' . $type . ': ' . print_r($matches, TRUE));
+//		syslog(1, ' > PREGMATCH ' . $type . ' Pattern: ' . $pattern);
+//		syslog(1, ' >>>>>>>>>>>>>>>>>>>>>>>>');
+//		}
+
+		// If we had results, return them.
+		if (isset($matches[$answer_position])) {
+			$results = array();
+
+			if (!is_array($matches[$answer_position])) {
+				foreach($matches AS $pos => $data) {
+					$matches[$pos] = array($data);
+				}
+			}
+
+			foreach ($matches[$answer_position] AS $key => $data) {
+
+				if ($filter_short === TRUE && strlen($data) < 32) {
+					syslog(1, ' > <!> LENGTH FILTER -- Excluding: ' . $data);
+					continue;
+				}
+
+				// Check the complete tag for any hints.
+				if ($my_hint = $this->findHintTags($matches[0][$key])) {
+					$results[$key]['hints'] = $my_hint;
+				}
+
+				if ($matches[1][$key]) {
+					$results[$key]['context'] = trim($matches[1][$key]);
+				}
+
+				// Add match to $results[$key]['response'].
+				$results[$key]['response'] = $data;
+			}
+
+			return $results;
+		}
+		else {
+			syslog(1, 'No matches for ' . $type . ' at location: ' . $answer_position . ' ? Body size is: ' . strlen($body));
+			syslog(1, print_r($matches, TRUE));
+		}
+
+		return false;
+	}
+
+
+	/**
+	 * Find the first hints="" syntax for a string, and return it.
+	 *
+	 * @return string | boolean
+	 *   False if no results, or a string of the contents of the first hint tag
+	 *   found in the string.
+	 */
+	protected function findHintTags($string) {
+		$pattern_hint = "#<.*hints=(?:\"|')([^\"']*)(?:'|\")?>#is";
+
+		if (isset($string)) {
+			preg_match($pattern_hint, $string, $matches);
+			if ($matches[1]) {
+				return $matches[1];
+			}
+		}
+		return false;
+	}
+
+
+	protected function weightResponseTags($tags, $terms) {
+
+		// Score card:
+		// h1 = 3
+		// h2 = 2
+		// h3 || alexa = 1
+		$score_chart = array(
+			'h1' => 15,
+			'h2' => 10,
+			'h3' => 5,
+			'alexa' => 5,
+			'bonus_score_all_terms' => 25
+		);
+		$response_score = array();
+		$top_score = 0;
+		$top_scorer = null;
+
+		foreach ($tags AS $type => $t_data) {
+			$response_score[$type] = array();
+			if (is_array($t_data)) {
+				foreach ($t_data AS $key => $t) {
+					$response_score[$type][$key] = 0;
+
+					$all_my_terms = true;
+
+					syslog(1, ' --> Scoring Response: ' . print_r($tags[$type][$key]['response'], TRUE));
+
+					// For each terms, find the number of occurances,
+					// and talley a score for this response.
+					foreach ($terms AS $term => $weight) {
+
+						$found_my_term = false;
+
+						// Response
+						$response_score[$type][$key] += $this->getResponseTermScore($t['response'], $term, $weight, 1, $found_my_term);
+
+						// Hints
+						if (isset($t['hints'])) {
+							$response_score[$type][$key] += $this->getResponseTermScore($t['hints'], $term, $weight, 2, $found_my_term);
+						}
+
+						// Context
+						if (isset($t['context'])) {
+							$response_score[$type][$key] += $this->getResponseTermScore($t['context'], $term, $weight, .5, $found_my_term);
+						}
+
+						if ($found_my_term === false && $weight > 1) {
+							$all_my_terms = false;
+						}
+					}
+
+					// If the score is more than mediocre, add weights based on the tag type.
+					if ($response_score[$type][$key] > 5 && isset($score_chart[$type])) {
+						syslog(1, '   >> ' . $type . ' Weight Bonus: +' . $score_chart[$type]);
+						$response_score[$type][$key] += $score_chart[$type];
+					}
+					// If all major terms matched the response, add a bonus.
+					if ($all_my_terms === true) {
+						syslog(1, '   >> <!!!> Response had all terms. Bonus: +' . $bonus_score_all_terms);
+						$response_score[$type][$key] += $score_chart['bonus_score_all_terms'];
+					}
+
+					syslog(1, '  >> Final: ' . $response_score[$type][$key]);
+					syslog(1, '');
+
+
+					// Keep track of the top score.
+					if ($response_score[$type][$key] > $top_score) {
+						$top_score = $response_score[$type][$key];
+						$top_scorer = $tags[$type][$key];
+					}
+				}
+			}
+			else {
+				syslog(1, ' > ' . $term . ' > ' . $type . ' >      <!> NONE');
+			}
+		}
+
+		syslog(1, '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+		syslog(1, 'Final Scores: ' . print_r($response_score, TRUE));
+
+		return $top_scorer;
+	}
+
+
+	protected function getResponseTermScore($haystack, $needle, $weight, $multiplier, &$found_term) {
+		$score = 0;
+		if ($my_count = substr_count(strtolower($haystack), strtolower($needle))) {
+			$score = ($my_count * $weight) * $multiplier;
+			syslog(1, '  -> ' . $needle . ' x' . $my_count . ' | +' . $score);
+			$found_term = true;
+			return $score;
+		}
+
+		$found_term = false;
+		return $score;
 	}
 
 
